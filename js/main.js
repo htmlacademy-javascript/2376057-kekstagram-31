6d96545fa25b90c50372b846a6a6b628e76cd57f
bobchinskiy <bobchisnkiy@gmail.com>
2024-03-11 16:19:50 +0500

    Доделал генератор

--- js/main.js
+++ js/main.js
@@ -1,47 +1,78 @@
-const name = [
-  'Соня',
-  'Алиса',
-  'Василий',
-  'Олег',
-  'Лариса',
-  'Лев',
-  'Надежда',
-  'Любовь',
-  'Вера',
-  'Костя',
-];
-
-const description = [
-  'Очень красиво получилось!',
-  'Ракета, бомба, петарда!',
-  'Годная темка',
-  'Я как всегда шикарен!',
-  'А это платно?',
-  'Яб тоже хотел так.',
-  'Пока это лучшее, что я видел',
-  'Как вам это?',
-  'Я бы немного пересмотрел данное действие',
-  'Ну вообще круто, но не очень',
-];
-
-const message = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
-];
-
-// Функция для рандомного количества лайков
-const generateRandomLikes = function () {
-  return Math.floor(Math.random() * (200 - 15 + 1)) + 15;
+// Хочу сказать спасибо Владиславу, ибо из-за спешки и отставания я опирался на его код и разбирал его. Thx
+//Функция рандомного числа
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.ceil(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 };
-const randomLikes = generateRandomLikes();
 
-// Функция для рандомного количества комментов
-const generateRandomComments = function () {
-  return Math.floor(Math.random() * (30 - 0 + 1));
+//Возвращение рандомного элемента массива
+const getRandomArrayElement = (element) => element[getRandomInteger(0, element.length - 1)];
+
+//Рандомный id
+const getRandomIdGenerator = (min, max) => {
+  const ids = [];
+  return function () {
+    let id = getRandomInteger(min, max);
+    while (ids.includes(id)) {
+      id = getRandomInteger(min, max);
+    }
+    ids.push(id);
+    return id;
+  };
 };
-const randomComments = generateRandomComments();
 
+
+const getRandomPhotoId = getRandomIdGenerator(1, 25);
+const getRandomMessageId = getRandomIdGenerator(1, 1000);
+
+//Создание коммента
+const getComment = () => {
+  const id = getRandomMessageId();
+  const avatarNumber = getRandomInteger(1, 6);
+  const message = [
+    'Всё отлично!',
+    'В целом всё неплохо. Но не всё.',
+    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+  ];
+  const name = [
+    'Соня',
+    'Алиса',
+    'Василий',
+    'Олег',
+    'Лариса',
+    'Лев',
+    'Надежда',
+    'Любовь',
+    'Вера',
+    'Костя',
+  ];
+  return {
+    id: id,
+    avatar: `img/avatar-${avatarNumber}.svg`,
+    message: getRandomArrayElement(message),
+    name: getRandomArrayElement(name)
+  };
+};
+
+// создание фото
+const getPhoto = () => {
+  const id = getRandomPhotoId();
+  return {
+    id: id,
+    url: `photos/${id}.jpg`,
+    description: `Мое супер фото ${id}`,
+    likes: getRandomInteger(15, 200),
+    comments: Array.from({ length: getRandomInteger(0, 30) }, getComment)
+  };
+};
+
+const PHOTOS_COUNT = 25;
+
+const createPhotos = () => Array.from({ length: PHOTOS_COUNT }, getPhoto);
+
+createPhotos();
